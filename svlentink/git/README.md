c0630f13519159f822f8fbad1f3f07826cedad83
svlentink <sanderlentink@mdmail.nl>
2016-12-20 19:24:30 +0000

    git

---
+++ svlentink/git/README.md
@@ -0,0 +1,6 @@
+# Git
+
+Everything can be dockerized, even git.
+Why? Just because we can.
+See my other repo. [MacHacks](http://github.com/svlentink/machacks) on how it could be useful.
+
---
+++ svlentink/git/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+#echo entering $0
+
+HELPMSG="docker run -it --rm \
+  svlentink/git \
+  -v ~/myRepo:/git
+  'firstname surname' \
+  user@email.tld \
+  commit -m 'lorem ipsum'"
+
+[[ "$#" -lt 3 ]] \
+  && echo less than 3 parameters found, please do $HELPMSG \
+  && exit 1
+
+UNAME=$1; shift
+UMAIL=$1; shift
+
+git config --global user.email "$UMAIL"
+git config --global user.name "$UNAME"
+
+GITACTION=$@
+
+! [[ -d /git/.git || ($1 == "init" || $1 == "clone") ]] \
+  && echo "no git repo found at /git" && \
+  exit 1
+
+git $GITACTION
