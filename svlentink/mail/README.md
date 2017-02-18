49a0e719ec89fe128ccdce7b92f0afb17f16643b
svlentink <sanderlentink@mijndomein.nl>
2017-02-18 14:47:33 +0000

    body file for mail

--- svlentink/mail/README.md
+++ svlentink/mail/README.md
@@ -2,11 +2,21 @@
 
 [![](https://images.microbadger.com/badges/image/svlentink/mail.svg)](https://microbadger.com/images/svlentink/mail "Get your own image badge on microbadger.com")
 
-Send mail by running this container
+Send mail by running this container.
+You can pass the body with the container as a command or mount a volume (body file).
 
-```bash
-to=$(git config user.email) \
-&& from=from@example.com \
-&& subj='lorem ipsum' \
-&& docker run --rm -it svlentink/mail $to $from "$subj" and this is the body
+Example of `docker-compose.yml`
+```yaml
+version: '3'
+services:
+  main:
+    image: svlentink/mail
+    volumes:
+      - /tmp/my_email_body.txt:/body:ro
+    environment:
+      - TO_ADDR=to@addr.ess
+      - FROM_NAME=Alpha Beta
+      - FROM_ADDR=from@addr.ess
+      - SUBJ=subject line
+#    command: body context here, only when the body volume is not mounted
 ```
--- svlentink/mail/entrypoint.sh
+++ svlentink/mail/entrypoint.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 set -e
 
+BODYLOC=/body
+
 echo checking if port 25 is open
 nc -z -w1 smtp.gmail.com 25
 echo port 25 is open
 
 /etc/init.d/postfix start
 
-function sendmailfunc { #sendmail is already used! (/usr/sbin/sendmail)
-  local to=$1; shift
-  local from=$1; shift
-  local subj=$1; shift
-  local body="$@"
-  echo Entering $FUNCNAME with: to=$to, from=$from, subj=$subj, body=$body
+[[ -z "$TO_ADDR" ]] && echo Usage: see https://hub.docker.com/r/svlentink/mail && exit 1
 
-  [[ -z "$body" ]] && echo "ERROR: Empty body" && exit 1
+FROM_HEADER="From: $FROM_NAME <$FROM_ADDR>"
+echo "Starting to send message '$SUBJ' to $TO_ADDR ($FROM_HEADER)"
 
-  echo -e "$body" | mail -s "$subj" --debug-level 7 -a 'From:'$from $to
-  echo Running a sleep command so the mail can actually be send by the daemon
-  local i=1
-  while [ "$(sendmail -bp)" != "Mail queue is empty" ]; do
-    [[ $i == 10 ]] && echo Checked too many times, it just failed && exit 1
-    echo checking for $i th time
-    local i=$(($i+1))
-    sendmail -bp
-    sleep 1
-  done
-  echo Mail send
-}
+if [[ -f $BODYLOC ]]; then
+  cat $BODYLOC | mail -s "$SUBJ" --debug-level 7 -a "$FROM_HEADER" $TO_ADDR
+else
+  echo -e "$@" | mail -s "$SUBJ" --debug-level 7 -a "$FROM_HEADER" $TO_ADDR
+fi
 
-sendmailfunc "$@"
+echo Running a sleep command so the mail can actually be send by the daemon
+I=1
+while [ "$(sendmail -bp)" != "Mail queue is empty" ]; do
+  [[ $I == 10 ]] && echo Checked too many times, it just failed && exit 1
+  echo checking for $I th time
+  ((++I))
+  sendmail -bp
+  sleep 1
+done
+  
+echo Mail send
 exit 0
