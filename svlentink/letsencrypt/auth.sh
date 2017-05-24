39e30b69ddc7b049ba85e5bbb5e636181b8439d9
svlentink <sanderlentink@mijndomein.nl>
2017-05-24 06:34:01 +0000

    log msg from auth

--- svlentink/letsencrypt/auth.sh
+++ svlentink/letsencrypt/auth.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Certbot only shows stderr from this script
+echo 'Entering $0 CERTBOT_VALIDATION=$CERTBOT_VALIDATION CERTBOT_TOKEN=$CERTBOT_TOKEN $@' 1>&2
 CHALLENGEPATH=/tmp/challenge/.well-known/acme-challenge
 mkdir -p $CHALLENGEPATH
 echo $CERTBOT_VALIDATION > $CHALLENGEPATH/$CERTBOT_TOKEN
