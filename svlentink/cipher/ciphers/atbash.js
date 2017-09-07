40eb51a521254a3bab23dbd051f5b394e8b8fe52
svlentink <sv@lent.ink>
2017-09-07 14:33:13 +0200

    toLowerCase was missing

--- svlentink/cipher/ciphers/atbash.js
+++ svlentink/cipher/ciphers/atbash.js
@@ -1,13 +1,13 @@
 var fs = require('fs')
 var file = '/input.txt'
-  
+ 
 
 function cipher(str){
   var alphabet = "abcdefghijklmnopqrstuvwxyz";
   var result = '';
   for (var i=0;i<str.length;i++)
     if ( alphabet.indexOf(str[i].toLowerCase()) !== -1)
-      result += alphabet[25 - alphabet.indexOf(str[i])]
+      result += alphabet[25 - alphabet.indexOf(str[i].toLowerCase())]
     else
       result += str[i]
   return result
