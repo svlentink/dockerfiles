c2efdaf557d9790a48f548372df26e6baf1cf551
svlentink <sv@lent.ink>
2017-10-01 16:45:40 +0200

    working iptables

--- svlentink/ipfilter/README.md
+++ svlentink/ipfilter/README.md
@@ -1,45 +1,10 @@
-IN PROGRESS, DOES NOT WORK ATM.
+# IpFilter with basic auth (optional) and SSL
 
-# IpFilter
+Used for getting things only quickly while developing.
+Less secure than Iptables, but way faster when using docker.
 
-IpFilter based on IPTABLES (alpine).
+See [this](https://github.com/svlentink/dockerfiles/blob/master/svlentink/ipfilter/README.md) example.
 
-Since the default docker behavior is to
-[change your iptables](http://blog.viktorpetersson.com/post/101707677489/the-dangers-of-ufw-docker),
-we just fix it inside a docker container. (since editing host machines cannot be done in our `docker-compose.yml` file)
-
-## Example usage
-
-Firewall (this container) -> Nginx (some static website)
-
-```yaml
-version: '2'
-services:
-  firewall:
-    privileged: true
-    image: svlentink/ipfilter
-    ports:
-      - "8080:80"
-    environment:
-      - ALLOWED_IPS=127.0.0.1/30,8.8.8.8/32
-#      - ALLOWED_TCP_PORTS=80 443
-#      - ALLOWED_UDP_PORTS=123
-    networks:
-      - shared
-  cdn:
-    image: nginx:alpine
-    volumes:
-      - $PWD:/usr/share/nginx/html:ro
-    links:
-      - firewall
-    networks:
-      - shared
-    expose:
-      - 80
-networks:
-  shared:
-
-```
 
 ## Production
 
---
+++ svlentink/ipfilter/docker-entrypoint.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+CONFIG=/etc/nginx/conf.d/default.conf
+
+if [ -z "$APP_PORT" ]; then
+  echo Missing env variable APP_PORT
+  exit 1
+fi
+
+cat << EOF > $CONFIG
+upstream app_upstream {
+  server app:$APP_PORT;
+}
+server {
+  listen 4321      ssl http2 default_server;
+  listen [::]:4321 ssl http2 default_server;
+  server_name _;
+
+  ssl_certificate     /selfsigned.crt;
+  ssl_certificate_key /selfsigned.key;
+  ssl_dhparam         /dhparam.pem;
+  
+  ssl_protocols TLSv1.2;
+  add_header X-Frame-Options DENY;
+  
+  include /nginx-filter-options.conf;
+
+  location / {
+    proxy_pass http://app_upstream;
+  }
+}
+
+EOF
+
+if [ -n "$ALLOWED_IPS" ];then
+  echo ALLOWED_IPS is set
+  for iprange in $ALLOWED_IPS; do
+    echo "allow $iprange;" >> /nginx-filter-options.conf
+  done
+  echo "deny all;" >> /nginx-filter-options.conf
+fi
+if [ -d /passdir ]; then
+  echo Using basic auth
+  echo 'auth_basic "closed site"; auth_basic_user_file /passdir/.htpasswd;' \
+  >> /nginx-filter-options.conf
+fi
+
+cat /nginx-filter-options.conf
+
+genCert () {
+  openssl req -x509 \
+  -nodes \
+  -days 365 \
+  -newkey rsa:2048 \
+  -keyout /selfsigned.key \
+  -out /selfsigned.crt \
+  -batch \
+  -subj "/C=NL/ST=NoordHolland/L=Amsterdam/O=Unknown/OU=Development/CN=www.lent.ink"
+  
+  openssl dhparam \
+  -out /dhparam.pem \
+  1024
+}
+
+if ! [ -f /dhparam.pem ]; then
+  genCert
+fi
+
+# https://github.com/nginxinc/docker-nginx/blob/1d2e2ccae2f6e478f628f4091d8a5c36a122a157/mainline/alpine/Dockerfile#L143
+nginx -g "daemon off;"
