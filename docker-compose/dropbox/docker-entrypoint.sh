8fc49c6c7c0eacdd940a3606b09d922da577e065
svlentink <sv@lent.ink>
2019-02-09 20:43:44 +0000

    less frequent checks

--- docker-compose/dropbox/docker-entrypoint.sh
+++ docker-compose/dropbox/docker-entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
+
 dropboxd
 sleep 10
 dropbox-cli status
 sleep 10
+
 cd /root/Dropbox
 
 while true
 do
   dropbox-cli filestatus || echo ERROR dropbox not running
-  sleep 3600
+  sleep 36000 # very 10h
 done
-
