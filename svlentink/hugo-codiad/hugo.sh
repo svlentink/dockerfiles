62c793cbd34ccb809efb61896e3f7a0a4c7fb03b
svlentink <sv@lent.ink>
2019-02-18 15:27:15 +0000

    change dir

--- svlentink/hugo-codiad/hugo.sh
+++ svlentink/hugo-codiad/hugo.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+cd /code/workspace/hugo
+
 if ! ls /code/workspace/hugo/config.*; then
   echo 'No config found at /code/workspace/hugo/config.*, creating new examplesite'
   # https://gohugo.io/getting-started/quick-start
   cd /tmp
   hugo new site examplesite
   mv /tmp/examplesite/* /code/workspace/hugo
-  cd /code/workspace/hugo
   git init
   git submodule add https://github.com/budparr/gohugo-theme-ananke.git themes/ananke
   echo 'theme = "ananke"' >> config.toml
