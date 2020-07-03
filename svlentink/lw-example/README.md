a2a98bb934ae43912b60a64c0d5991cee3d2d887
SanderLentink.nl <sander@lent.ink>
2020-07-03 19:38:14 +0000

    added lw-example

---
+++ svlentink/lw-example/README.md
@@ -0,0 +1,2 @@
+see https://github.com/lipp/login-with/blob/master/example/token-decrypter
+
---
+++ svlentink/lw-example/index.js
@@ -0,0 +1,25 @@
+// https://github.com/lipp/login-with/blob/master/example/token-decrypter/index.js
+
+const jsonwebtoken = require('jsonwebtoken')
+const {send} = require('micro')
+const tokenSecret = process.env.LW_JWT_SECRET
+const cookie = require('cookie')
+const url = require('url')
+
+if (!tokenSecret) {
+  console.error('no LW_JWT_SECRET env variable specified')
+  process.exit(1)
+}
+
+module.exports = async (req, res) => {
+  const jwtCookie = cookie.parse(req.headers.cookie).jwt
+  if (req.headers.referer) {
+    const {protocol, hostname} = url.parse(req.headers.referer)
+    const origin = `${protocol}//${hostname}`
+    res.setHeader('Access-Control-Allow-Origin', origin)
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  }
+  send(res, 200, {jwt: jwtCookie, decrypted: jsonwebtoken.verify(jwtCookie, tokenSecret)})
+}
+
