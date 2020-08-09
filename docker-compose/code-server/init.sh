64d05940dccff46f8a972b36a98b0e3a2c861119
SanderLentink.nl <sander@lent.ink>
2020-08-09 16:51:28 +0200

    code-server added

---
+++ docker-compose/code-server/init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo Creating key for webide to access host
+mkdir -p /sshkey
+ssh-keygen -N '' -f /sshkey/id_rsa
+cat /sshkey/id_rsa.pub >> /root/.ssh/authorized_keys
+
+if [ ! -f passdir/.*passwd ]; then
+  read -p "Please provide username:" USERNAME
+  read -p "Pleas provide password:" PASSWORD
+  echo -n "$USERNAME:" > passdir/.htpasswd
+  openssl passwd -apr1" "$PASSWORD" >> passdir/.htpasswd
+fi
+
+
