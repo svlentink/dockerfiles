9855be24174bdea5c799c7ee55eb0ee87f04b0f4
SanderLentink.nl <sander@lent.ink>
2020-08-10 22:52:47 +0200

    structure change

--- docker-compose/code-server/init.sh
+++ docker-compose/code-server/init.sh
@@ -2,14 +2,6 @@
 
 echo Creating key for webide to access host
 mkdir -p sshkey
-
-cat << EOF > sshkey/login-to-host.sh
-#!/bin/sh
-ssh -i /sshkey/id_pub root@172.17.0.1
-EOF
-
-chmod +x sshkey/login-to-host.sh
-
 ssh-keygen -N '' -f sshkey/id_rsa
 cat sshkey/id_rsa.pub >> /root/.ssh/authorized_keys
 
@@ -17,7 +9,7 @@ if [ ! -f passdir/.*passwd ]; then
   read -p "Please provide username:" USERNAME
   read -p "Pleas provide password:" PASSWORD
   echo -n "$USERNAME:" > passdir/.htpasswd
-  openssl passwd -apr1" "$PASSWORD" >> passdir/.htpasswd
+  openssl passwd -apr1 "$PASSWORD" >> passdir/.htpasswd
 fi
 
 
---
+++ docker-compose/code-server/login-to-host.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ssh -i /sshkey/id_pub root@172.17.0.1
